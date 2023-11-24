0f037cce947858109286598a250d57532f5a6dcd
Tolga Boroglu <46046034+tolgaboroglu@users.noreply.github.com>
2023-11-25 01:32:05 +0300

    update_wkh

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ async def generate_image(request: Request,html_variable :HtmlTemplateModel):
             'quiet': ''
         }
 
-        config = imgkit.config(wkhtmltoimage='C:/Program Files/wkhtmltopdf/bin/wkhtmltoimage.exe')
+        config = imgkit.config(wkhtmltoimage='wkhtmltopdf/bin/wkhtmltoimage.exe')
         # Use the config in your imgkit.from_string() call
         img = imgkit.from_string(html_content, False, options=options, config=config)
         
